ca7bd0abade764ad6dfe2fa119f911034b9dd974
Jongho Moon <ansdjxod90@gmail.com>
2022-12-16 15:42:36 +0900

    Edit db_setting.py

--- project-v2/db_setting.py
+++ project-v2/db_setting.py
@@ -4,7 +4,7 @@ DATABASES = {
         'NAME': 'travelinfo',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': 'db-svc.default.svc.cluster.local',
+        'HOST': '192.168.119.151',
         'PORT': '3306',
     }
 }
