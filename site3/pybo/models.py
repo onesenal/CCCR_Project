099bd303359333f7a51e21368fa491d716f57e10
onesenal <119921476+onesenal@users.noreply.github.com>
2022-12-26 15:06:45 +0900

    Update models.py

--- site3/pybo/models.py
+++ site3/pybo/models.py
@@ -6,13 +6,13 @@ from django.db import models
 class world(models.Model):
     CONTINENT = models.CharField(max_length=100)
     COUNTRY = models.CharField(max_length=100)
-    p1 = models.CharField(max_length=150, null=True)
-    p2 = models.CharField(max_length=150, null=True)
-    p3 = models.CharField(max_length=150, null=True)
-    p4 = models.CharField(max_length=150, null=True)
-    p5 = models.CharField(max_length=150, null=True)
-    p6 = models.CharField(max_length=150, null=True)
-    p7 = models.CharField(max_length=150, null=True)
-    p8 = models.CharField(max_length=150, null=True)
+    p1 = models.CharField(max_length=500, null=True)
+    p2 = models.CharField(max_length=500, null=True)
+    p3 = models.CharField(max_length=500, null=True)
+    p4 = models.CharField(max_length=500, null=True)
+    p5 = models.CharField(max_length=500, null=True)
+    p6 = models.CharField(max_length=500, null=True)
+    p7 = models.CharField(max_length=500, null=True)
+    p8 = models.CharField(max_length=500, null=True)
 
     
